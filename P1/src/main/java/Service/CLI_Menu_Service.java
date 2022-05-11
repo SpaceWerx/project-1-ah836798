c3944c69b877c2d94b06383d85831df2385102e0
ah836798 <arlontus@hotmail.com>
2022-05-11 18:32:00 -0500

    CLI Menu Service 2nd upload

--- P1/src/main/java/Service/CLI_Menu_Service.java
+++ P1/src/main/java/Service/CLI_Menu_Service.java
@@ -3,6 +3,7 @@ package Service;
 import java.util.*;
 import java.util.Scanner; // import the Scanner class
 import Models.Roles;
+import Models.Status;
 
 
 public class CLI_Menu_Service {
@@ -10,11 +11,12 @@ public class CLI_Menu_Service {
 	//CLI Menu is the Command Line Interface Menu for the 
 		//initialize a new CLI Menu Service 
 	      public static void main(String[] args) {
-	        Scanner cliMenu1 = new Scanner(System.in);
-	        private int validEntries;	        
-	        private int firstChoice;
+	    	  
+	      }
+//       private int validEntries;	        
+//       private int firstChoice;
 	        
-	       
+//Display Main Menu Method//	       
 public void displayMenu() {
 	boolean menuOptions = true; //Using this to let the menu continue after user input
 	
@@ -35,10 +37,11 @@ public void displayMenu() {
 		System.out.println("0 -> Exit Application");
 		
 		// The user chooses a menu option and the scanner takes the input and put it into an int variable
+		
+//Scanner firstChoice = new Scanner(System.in);
 		// Calls the promptSelection() helper method to handle validation
 		// The parameteres list the valid options that the user must choose from  
-		int firstChoice = promptSelection(validEntries:  1, 2, 0);
-		
+		int firstChoice = promptSelection( ...validEntries: 1, 2, 0);
 		
 		// Takes the user input and the switch statement executes the appropriate code
 		switch (firstChoice) {
@@ -53,13 +56,371 @@ public void displayMenu() {
 		case 0:   
 			System.out.println("\nHave a great day! Goodbye.");
 			menuOptions = false;
-			break;
-			
-		}
-		
+			break;		
+		}	
 	}//end of while loop
 }
-	      }
-	      
 
+
+//Display Employee Menu Method//
+
+public void displayEmployeeMenu(User employee) {
+	boolean employeePortal = true;
+	
+	System.out.println("--------------------------------------------------------");
+	System.out.println("Welcome to the Employee Portal, " + employee.getUsername());
+    System.out.println("--------------------------------------------------------");
+    System.out.println();
+    
+    while (employeePortal) {
+    	System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+    	System.out.println("1 -> View Previous Requests");
+    	System.out.println("2 -> Submit a Reimbursement");
+    	System.out.println("0 -> Return to Main Menu");
+    
+    	//user chooses a menu option and the scanner takes the input and put it into a int variable
+    	int firstChoice = promptSelection(...validEntries: 1,2,0);
+    	
+    	switch (firstChoice) {
+    	    case 1:
+    	    	displayPreviousRequests(employee);
+    	    	break;
+    	    case 2:
+    	    	submitReimbursement(employee);
+    	    	break;
+    	    case 0:
+    	    	System.out.println("Returning to Main Menu...");
+    	    	employeePortal = false;
+    	    	break;
+    	    	
+    	}
+        
+    }
+}
+
+
+//Display Manager Menu Method//	
+public void displayFinanceManagerMenu(User employee) {
+	boolean managerPortal = true;
+	
+	System.out.println("--------------------------------------------------------");
+	System.out.println("Welcome to the Manager Portal, " + manager.getUsername());
+    System.out.println("--------------------------------------------------------");
+    System.out.println();
+    
+    while (managerPortal) {
+    	System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+    	System.out.println("1 -> View All Pending Reimbursements");
+    	System.out.println("2 -> View All Resolved Reimbursements");
+    	System.out.println("3 -> Process a Reimbursement");
+    	System.out.println("0 -> Return to Main Menu");
+    
+    	//user chooses a menu option and the scanner takes the input and put it into a int variable
+    	int firstChoice = promptSelection(...validEntries: 1, 2, 3, 0);
+    	
+    	switch (firstChoice) {
+    	    case 1:
+    	    	displayPendingReimbursements();
+    	    	break;
+    	    case 2:
+    	    	displayResolvedReimbursements();
+    	    	break;
+    	    case 3:
+    	    	processReimbursement(manager);
+    	    	
+    	    case 0:
+    	    	System.out.println("Returning to Main Menu...");
+    	    	managerPortal = false;
+    	    	break;
+    	    	
+    	}
+        
+    }
+}
+
+//Fetch Input Helper Method//
+
+/** 
+ * Prompts the user for input, ignoring everything after the first whitepspace.
+ * For example, "123 456" will be converted to "123", and the " 456" will be discarded.
+ * This is known as the first "token" of the user's input.
+ * 
+ * @return the first token of user input on the next line
+ */
+
+public String fetchInput() {
+	// scan.nextLine() obtains the entire line, such as "123 456"
+	//split() turns it into an array separated by whitespace, such as {"123", "456"}
+	// [0] keeps only the first element, leaving "123" 
+	return scan.nextLine().split( regex: " ")[0];
+}
+
+//Prompt Selection Helper Method//
+
+/**
+ * Prompts the user for input.<br>
+ * If the provided input is not a valid option, the user will be prompted again until a valid answer is provided.
+ *
+ * @param validEntries the valid int values that the user must choose from 
+ * @return the selected option as an int
+ */
+
+public int promptSelection(int ...validEntries) {
+	int input; 
+	boolean valid = false; // flag to track if the input matched a valid entry
+	
+	do { // do-while loop to continue to prompt user until the response is valide
+		// process the input
+		input = parseIntegerInput(fetchInput());
+		
+		// Check if the input matches a valid entry
+		for (int entry : validEntries) {
+			if (entry == input) {
+				//if it does, we activate the flag, which will end the do-while loop
+				valid = true;
+				break; //stop searching for-loop since we have already found the match
+				
+			}
+		}
+		// check if the input was not valid, so we can inform the user that we need a new input
+		if (!valid) {
+			System.out.println("Input received was not a valid option, please try again.");
 			
+		}
+	} while (!valid);
+	return input;
+	
+}
+
+//Parse Integer and Double Helper Methods//
+/** 
+ * Attempt to parse input from the user as an int to be useful for control-flow.
+ * Leverages the built-in Integer.parseInt method.
+ * Note: If the input is not a valid int, such as "hello, world",
+ * Integer. parseInt throws a NumberFormatException, which will need to be handled.
+ * 
+ * @param input the user input that will be parsed into an int
+ * @return the input as an int or zero if it was malformed
+ */
+public int parseIntegerInput(String input) {
+	try {
+		return Integer.parseInt(input);
+	} catch (NumberFormatException e) {	
+		System.out.println("The input received was malformed, please try again.");
+		return -1; //return -1 by default to be rejected by other validation contigencies
+	}
+}
+
+public double parseDoubleInput(String input) {
+	try {
+		return Double.parseDouble(input);
+	} catch (NumberFormatException e) {	
+		System.out.println("The input received was not valid, please try again.");
+		return -1; //return -1 by default to be rejected by other validation contigencies
+	}
+}
+
+//Handle Portal Helper Method//
+public void handlePortal(Role role) {
+    // get the List of employees from the repository layer
+	List<User> users = userService.getByRole(role);
+	
+	int[] ids = new Int[users.size() + 1];
+	ids[users.size()] = 0;
+	for (int i = 0; i < users.size(); i++) {
+		ids[i] = users.get(i0).getId();
+		
+	}
+	
+	// Ask for employee ID number to continue
+	System.out.println("----------------------------------------------------------");
+	System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+	
+	// Enhance for loop to print out all users one by one
+	for (User u : users) {
+		System.out.println(u.getId() + " -> " + u.getUsername());
+	}
+	System.out.println("0 -> Return to Main Menu");
+	System.out.println();
+	
+	int userChoice = promptSelection(ids);
+	
+	if (userChoice == 0) {
+		return;
+	}
+	User employee = userService.getUserById(userChoice);
+	
+	if (role == Role.Manager) {
+		System.out.println("Opening Manager Portal for " + employee.getUsername());
+		displayFinanceManagerMenu(employee);
+	} else {
+		System.out.println("Opening Employee Portal for " + employee.getUsername());
+		displayEmployeeMenu(employee);
+	
+	}
+}
+
+//Display Previous Requests helper method//
+public void displayPreviousRequests(User employee) {
+	List<Reimbursement> reimbursements = rService.getReimbursementsByAuthor(employee.getOd);
+	
+	if (reimbursements.isEmpy()) {
+		System.out.println("No Previous Requests...");
+		System.out.println("Returning to Previous Menu... ");
+	}
+	for (Reimbursement r : reimbursements) {
+		System.out.println(r);
+	}
+}
+
+//Submit Reimbursement helper method//
+
+public void submitReimbursement(User employee) {
+	Reimbursement reimbursementToBeSubmitted = new Reimbursement();
+	reimbursementToBeSubmitted.setAuthor(employee.getId));
+	
+	System.out.println(" What type or reimbursement would you like to submit");
+	System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+	System.out.println("1 -> Lodging");
+	System.out.println("2 -> Travel");
+	System.out.println("3 -> Food");
+	System.out.println("4 -> "Other");"
+    int typeDecision = promptSelction(.employee..validEntries: 1,2,3,4);
+	
+	switch (typeDecision) {
+    case 1:
+    	reimbursementToBeSubmitted.setType(ReimbursementType.Lodging));
+    	break;
+    case 2:
+    	reimbursementToBeSubmitted.setType(ReimbursementType.Travel));
+    	break;
+	case 3:
+		reimbursementToBeSubmitted.setType(ReimbursementType.Food));
+	break;
+	case 4:
+    	reimbursementToBeSubmitted.setType(ReimbursementType.Other));
+    	break;
+	}
+	
+	System.out.println("Please enter the dollar amount that you are requesting to be reimbursed");
+	System.out.println("$");
+	
+	reimbursementToBeSubmitted.setAmount(parseDoubleInput(fetchInput)));
+	if (reimbursementToBeSubmitted.getAmount() <= 0) {
+		System.out.println("Invalid Amount has been entered, please input a correct dollar amount"); 
+		boolean valid = false;
+		while (!valid) {
+			reimbursementToBeSubmitted.setAmount(parseDoubleInput(fetchInput)));
+			if (reimbursementToBeSubmitted.getAmount() != 0) { (parseDoubleInput(fetchInput)));)
+            valid = true;
+			}	
+		}	
+	}	
+	
+	System.out.println("Please enter a description/reason for you reimbursement request.");
+		
+	reimbursementToBeSubmitted.setDescription(scan.nextLine()));
+	if (reimbursementToBeSubmitted.getDescription().trim().equals("")) {
+		System.out.println("You cannot submit a request with an empty description, please explain the reason for your request.");		 
+		boolean valid = false;
+		while (!valid) {
+			reimbursementToBeSubmitted.setDescription(scan.nextLine()));
+			if (reimbursementToBeSubmitted.getDescription().trim().equals("")) {
+            valid = true;
+			}	
+		}	
+	}	
+	rService.submitReimbursement(reimbursementToBeSubmitted);
+	
+}
+
+//Display Reimbursement Method//
+public void displayPendingReimbursements() {
+	List<Reimbursement> pendingReimbursements = rService.getPendingReimbursements();
+
+	if (pendingReimbursements.isEmpty()) {
+		System.out.println("No Pending Requests...");
+		System.out.println("Returning to Previous Menu...");		
+	}
+	for (Reimbursement r : pendingReimbursements) {
+		System.out.println(r);
+	}
+	
+}
+
+public void displayPendingReimbursements() {
+	List<Reimbursement> resolvedReimbursements = rService.getResolvedReimbursements();
+
+	if (resolvedReimbursements.isEmpty()) {
+		System.out.println("No Resolved Requests...");
+		System.out.println("Returning to Previous Menu...");		
+	}
+	for (Reimbursement r : resolvedReimbursements) {
+		System.out.println(r);
+	}
+	
+}
+
+//Process Reimbursement Helper Method//
+public void processReimbursement(User manager) {
+	boolean processPortal = true;
+	
+	System.out.println("--------------------------------------------------------");
+	System.out.println("Welcome to the Manager Processing Portal, " + manager.getUsername());
+    System.out.println("--------------------------------------------------------");
+    System.out.println();
+    
+    while (processPortal) {
+    	List<Reimbursement> reimbursements = rService.getPendingReimbursements();
+    
+    	if (reimbursement.isEmpty()) {
+    		System.out.println("There are no reimbursements to process.");
+    		System.out.println("Returning to previous menu...");
+    		return;
+    	}
+    	
+    	int[] ids =new int[reimbursements.size()];
+    	for (int i = 0; i< reimbursements.size(); i++) {
+    		Reimbursement r = reimbursements.get(i);
+    		User author = userService.getUserById(r.getAuthor());
+    		System.out.println(r.getId() + " -> " author.getUsername() + " : $" + r.getAmount());
+    		ids[i] = r.getId();
+    		
+    	}
+    	
+    	System.out.println("Please enter the ID of the Reimbursement you wish to process.");
+    	
+    	int selection = promptSelection(ids);
+    	Reimbursement reimbursementToBeProcessed = rService.getReimbursementById(selection);
+        System.out.println("Processing reimbursement #" +reimbursementToBeProcessed.getId());
+        System.out.println("Details\nAuthor: "
+        		+ userService.getUserById(reimbursementToBeProcessed.getAuthor()).getUsername()
+        		+ "\nAmount: " + reimbursementToBeProcessed.getAmount()
+    	        + "\nDescription: " + reimbursementToBeProcessed.getDescription()
+    	        
+    	);
+        
+        System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+        System.out.println("1 -> Approve");
+        System.out.println("2 -> Deny");
+        
+        int decision = promptSelection( ...validEntries: 1,2);
+        Status status = (decision == 1) ? Status.Approved : Status.Denied;
+        rService.update(reimbursementToBeProcessed, manager.getId(), status);
+        
+        System.out.println("Would you like to process another reimbursememt?");
+        System.out.println("PLEASE ENTER THE NUMBER OF YOUR CHOICE");
+        System.out.println("1 -> Yes");
+        System.out.println("2 -> No");
+        
+        int lastChoice = promptSelection(...validEntries: 1,2);
+        
+        if (lastChoice ==2) {
+        	processPortal = false;
+        }  	        
+    }
+
+
+
+
+
