8e50ac8f2c74e657eb47e1cbb849670af1d1717c
ah836798 <arlontus@hotmail.com>
2022-05-11 14:04:07 -0500

    Reimbursement type enum upload

--- P1/src/main/java/Models/Status.java
+++ P1/src/main/java/Models/Status.java
@@ -1,6 +1,7 @@
 package Models;
 
 import Models.Reimbursement_Model;
+import java.util.Scanner;
 
 
 public enum Status {
@@ -17,29 +18,26 @@ public enum Status {
 	//	Pending -> Denied
 //		//Reimbursements ***CANNOT*** go from Denied to Approved or vice versa.//
 	
-     
-	  public static void main(String []args) {
-		 
-		  
+	  public static void main(String []args) {		  
 		 Status options =Status.Pending;
-		 
-		 
 		 
 		 switch(options){
 			
 		 case Pending:
+			 
 		 System.out.println("Pending");
 		 break;
 		 
 		 case Approved:
+			 
 	     System.out.println("Approved");
 	     break;
 	     
 	     default:
+	    	
 	     System.out.println("Denied");
 		
 		 }
 	  }	
 }
 
-}
